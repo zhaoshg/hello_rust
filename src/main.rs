8c8568434dafa55cbbae592a3dba0c31b4cc3840
zhaoshg <mr.zhaoshg@gmail.com>
2024-04-06 23:29:03 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,8 @@ string_from();
 fn string_from(){
     let mut s1 = String::from("hello");
     s1.push_str(" world!");
-    let s2 = s1;
+    // let s2 = s1;
+    let s2 = s1.clone();
     println!("{}", s1);
 }
 
